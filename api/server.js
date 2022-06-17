d7a4d7a2f3e02993adfa23c32d371bffd281a68d
Will Boyd <willb@linuxacademy.com>
2022-06-17 12:42:46 -0400

    tweak server code

--- api/server.js
+++ api/server.js
@@ -5,22 +5,29 @@ var mysql = require('mysql');
 
 app.use(cors());
 
-var pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "plantshop"
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "plantshop",
 });
 
-app.get('/', (req, res)=> {
-  pool.getConnection(function(err, connection) {
-    if (err) throw err;
-    connection.query("SELECT * FROM items", function (error, result, fields) {
-      connection.release();
-      if (error) throw error;
-      res.json({items: result.map(v => Object.assign({}, v))});
+app.get('/', (req, res, next)=> {
+    connection.query('select * from items', function (error, results, fields) {
+        if (error) {
+            connection.destroy();
+            throw error;
+        } else {
+            // connected!
+            console.log("Query result:");
+            console.log(results);
+            if (error) {
+                next (error);
+            } else {
+                res.json({items: results.map(v => Object.assign({}, v))});
+            }
+        }
     });
-  });
 });
 
 app.listen(8080, ()=> console.log("App is running on port 8080"));
